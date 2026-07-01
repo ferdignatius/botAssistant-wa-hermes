2e1c956cf8d990dcec1318305aa92a245b3b6ba2
ferdignatius <ferdignatius@gmail.com>
2026-07-01 09:28:22 +0700

    v2.0.9 - fixing url database

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,11 @@
-import 'dotenv/config';
-import { defineConfig, env } from 'prisma/config';
+import { defineConfig } from 'prisma/config';
 
-// Prisma v7 config — DATABASE_URL dibaca di sini, bukan di schema.prisma
+// Prisma v7 config
 export default defineConfig({
     schema: './prisma/schema.prisma',
-    datasource: {
-        url: env('DATABASE_URL'),
-    },
+    migrations: {
+        path: './prisma/migrations',
+        seed: './prisma/seed.ts',
+        // Tidak perlu dataSource, karena akan dibaca dari env.DATABASE_URL
+    }
 });
